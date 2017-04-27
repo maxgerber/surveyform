3d817c5dc9cc4d81d8e069a45f056c311993d14e
Max Gerber <mg14859@my.bristol.ac.uk>
2017-04-27 22:12:02 +0100

    Done minus gender trouble

--- script.js
+++ script.js
@@ -1,26 +1,48 @@
 window.addEventListener("DOMContentLoaded", function() {
     var party = document.getElementById("partyvote");
-
+    var button = document.getElementById("button");
+    var otherBool = document.getElementById("othercheck");
+    var otherText = document.getElementById("othertext");
     party.addEventListener("change", function() {
         switch(party.selectedIndex) {
             case 0:
                 document.body.style.backgroundColor = "#A9D7D1";
+                button.style.backgroundColor = "#A9D7D1";
+                button.style.color = "black";
                 break;
             case 1:
                 document.body.style.backgroundColor = "#DC241F";
+                button.style.backgroundColor = "#DC241F";
+                button.style.color = "white";
                 break;
             case 2:
                 document.body.style.backgroundColor = "#0087DC";
+                button.style.backgroundColor = "#0087DC";
+                button.style.color = "white";
                 break;
             case 3:
                 document.body.style.backgroundColor = "#FAA61A";
+                button.style.backgroundColor = "#FAA61A";
+                button.style.color = "black";
                 break;
             case 4:
                 document.body.style.backgroundColor = "#6AB023";
+                button.style.backgroundColor = "#6AB023";
+                button.style.color = "white";
                 break;
             case 5:
                 document.body.style.backgroundColor = "#BABABA";
+                button.style.backgroundColor = "#BABABA";
+                button.style.color = "black";
                 break;
     }
+  });
+
+  otherBool.addEventListener("change", function() {
+     if ( !otherBool.checked ) {
+         otherText.classList.add("hidden");
+     } else {
+         otherText.classList.remove("hidden");
+     }
   });
 });
